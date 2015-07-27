9d5efb1557b21206146406d2f5935f9de84c23cf
gurmanbh <gurmanbhatia.93@gmail.com>
2015-07-27 02:12:15 -0400

    fixed tooltips

--- js/main.js
+++ js/main.js
@@ -4,17 +4,33 @@
 		
 		$('.box').on('mousemove', function(e){
 
-		    var buffer = 40;
-		    var tooltip_height = $('#tooltip').height();
+		    var buffer = 55;
+		    arrowbuffer = 5;
+		    var tooltip_height= $('#tooltip').height();
 		    var tooltip_width = $('#tooltip').width();
 		    $('#tooltip').css({
-		        top: e.pageY - tooltip_height - buffer,
-		        left: e.pageX - tooltip_width / 2
+		        top: e.pageY - tooltip_height / 2,
+		        left: e.pageX + tooltip_width/2 - buffer
 		    })
 		    $('#tooltip').addClass('visible');
 		    var usingid = $(this).attr('data-id');
 		    baketooltip(usingid);
 		    $(this).addClass('active');
+		    console.log(tooltip_height)
+
+		    $('#tail1').css({
+		    	bottom:tooltip_height/2-arrowbuffer,
+		    	left:-28
+		    })
+		    $('#tail2').css({
+		    	bottom:tooltip_height/2-arrowbuffer-1,
+		    	left:-28
+		    })
+		    $('#tailShadow').css ({
+			    bottom:tooltip_height/2-arrowbuffer+5,
+			    left: -14
+			})
+
 		});
 		$('.box').on('mouseout', function(e){
 		    $('#tooltip').removeClass('visible');
@@ -30,7 +46,7 @@
 		})
 		function baketooltip(id){
 			var obj = _.findWhere(dataset,{'id':id});
-			$('.headline').html('<a href="'+obj.link+'">'+obj.headline.split('|')[0].trim()+'</a>')
+			$('.headline').html('<a target="_blank" href="'+obj.link+'">'+obj.headline.split('|')[0].trim()+'</a>')
 			$('.section').html('was published in the '+obj.publishedin+' section.')
 		}
 	});
